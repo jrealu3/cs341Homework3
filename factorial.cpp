35a686cf941b1092ad079b947c562592cea27ee8
joshua realubit <47401167+jrealu3@users.noreply.github.com>
2021-09-29 10:40:50 -0500

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,11 @@
+//Joshua Realubit
+//netID - jrealu3
+#include "https://github.com/sercantutar/infint/blob/master/InfInt.h"
+
+long factorial (int n){
+  long result = 1;
+  while(n > 1){
+    result *= n--;
+  }
+  return result;
+}
